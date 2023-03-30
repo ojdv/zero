d12a2bd6ca77406a6718412af286f45c02b4b0f1
aurorads <126377954+ojdv@users.noreply.github.com>
2023-03-30 18:53:50 +0800

    Delete a004.py

--- a004.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        y = int(input())
-        if ((y % 4 == 0) and (y % 100 != 0)) or (y % 400 == 0):
-            print("閏年")
-        else:
-            print("平年")
-    except:
-        break
